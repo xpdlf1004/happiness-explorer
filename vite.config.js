77d88e9e981bd8f2ecca7613868fe6213c77451c
xpdlf1004 <xpdlf1004@gmail.com>
2025-11-09 22:37:07 +0900

    Add allowedHosts.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  allowedHosts: ['proj1.borihopang.com'],
+  preview: {
+    allowedHosts: ['*'],
+  }
 })
